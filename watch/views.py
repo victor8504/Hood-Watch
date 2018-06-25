fa4caa5f051a159b161b0177ef25f28decac6a1e
victor njuguna <victormoore254@gmail.com>
2018-06-25 12:05:11 +0300

    Sign-up * write signup url * write the view function for the signup url * create the sign up form * create the registration base and signup templates

--- watch/views.py
+++ watch/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm
+from .emails import send_activation_email
+from .tokens import account_activation_token
+from .models import Profile
 
 # Create your views here.
 def home(request):
     title = 'Hood-Watch'
 
     return render(request, 'index.html',{'title':title})
+
+
+def signup(request):
+    if request.user.is_authenticated():
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+                current_site = get_current_site(request)
+                to_email = form.cleaned_data.get('email')
+                send_activation_email(user, current_site, to_email)
+                return HttpResponse('Confirm your email address to complete registration')
+        else:
+            form = SignupForm()
+            return render(request, 'registration/signup.html',{'form':form})
