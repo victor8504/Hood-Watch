c002020afb063f6d557ca8ee79029271c38eb032
victor njuguna <victormoore254@gmail.com>
2018-06-25 12:09:35 +0300

    Activation * write the activation url * define the view function for the activation url

--- watch/urls.py
+++ watch/urls.py
@@ -6,4 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$',views.home, name='home'),
     url(r'^$',views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- watch/views.py
+++ watch/views.py
@@ -36,3 +36,19 @@ def signup(request):
         else:
             form = SignupForm()
             return render(request, 'registration/signup.html',{'form':form})
+
+    
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return HttpResponse('Thank you for confirming email. Now login to your account')
+    else:
+        return HttpResponse('Activation link is invalid')
