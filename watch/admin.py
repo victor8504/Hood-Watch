1595c3a1091252a58696b309e7929e72ccd39633
victor njuguna <victormoore254@gmail.com>
2018-06-26 12:16:07 +0300

    Admin * create a custom HoodClass * register both the new custom HoodClass and the Hood Model to the Admin dashboard

--- watch/admin.py
+++ watch/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
+from django.forms.widgets import TextInput
+
+from django_google_maps.widgets import GoogleMapsAddressWidget
+from django_google_maps.fields import AddressField, GeoLocationField
+
 from .models import Profile
 
 # Register your models here.
 admin.site.register(Profile)
+
+
+class HoodCustom(admin.ModelAdmin):
+    formfield_overrides = {
+        AddressField:{
+            'widget': GoogleMapsAddressWidget
+        },
+        GeoLocationField:{
+            'widget': TextInput(attrs={
+                'readonly':'readonly'
+            })
+        }
+    }
+
+admin.site.register(Hood, HoodCustom)
